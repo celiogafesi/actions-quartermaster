cff9f0c02ed4e3ac86afcd13bf9d2cdcb40520cb
Soren Mathiasen <smo@tradeshift.com>
2020-10-28 09:34:45 +0100

    Support helm3

--- update/entrypoint.sh
+++ update/entrypoint.sh
@@ -6,12 +6,24 @@ update_charts_dependencies() {
 
   helm init --client-only
   for chart_path in ${CHARTS_DIR}/*; do
+      HELM=helm
+      if is_helm3 ${chart_path}; then
+	  HELM=helm3
+      fi
     if [[ "${chart_path}/requirements.yaml" && ! -d "${chart_path}/charts" ]]; then
-      helm dependencies update ${chart_path}
+      $HELM dependencies update ${chart_path}
     else
       echo "No external dependencies found for ${chart_path}."
     fi
   done
+}
+
+is_helm3() {
+  local CHART_PATH=$1
+  if [[ $(cat "${CHART_PATH}/Chart.yaml" | grep -E "apiVersion: .?v2") ]]; then
+    return 0
+  fi
+  return 1
 }
 
 update_charts_dependencies "${PROJECT_DIR}"
